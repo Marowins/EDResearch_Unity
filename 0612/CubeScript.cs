d4c9a47c96107d4cf7e324721735db06eb946b6a
Marowins <gamemaro@naver.com>
2017-11-22 23:08:38 +0900

    Rigidbody

---
+++ 0612/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if ( Input.GetKeyDown( KeyCode.UpArrow )) {
+			this.gameObject.transform.Translate (this.gameObject.transform.forward * 5f * Time.deltaTime );
+		}
+		if ( Input.GetKeyDown( KeyCode.DownArrow )) {
+			this.gameObject.GetComponent<Rigidbody> ().AddForce (this.gameObject.transform.forward * 200);
+		}
+	}
+}
